e214f4b73832eed32b5b3a78cbdef3c107b2240b
Peter Barcsak <peter.barcsak@gmail.com>
2017-06-14 16:55:32 +0100

    Game is able to turn switches

--- model/game.js
+++ model/game.js
@@ -3,6 +3,11 @@ const Player = require('./player')
 const Game = function() {
     this.player1 = new Player();
     this.player2 = new Player();
+    this.turn = this.player1
+}
+
+Game.prototype.turnSwitch = function() {
+    this.turn = ( this.turn === this.player1 ) ? this.player2 : this.player1
 }
 
 module.exports = Game;
--- test/gameSpec.js
+++ test/gameSpec.js
@@ -9,4 +9,12 @@ describe('Game', function() {
         expect(game.player1).to.have.property('claimedFields')
         expect(game.player2).to.have.property('claimedFields')
     })
+
+    describe('Switch Turn', function() {
+        it('changes the turns between the players', function() {
+            game.turn = game.player1
+            game.turnSwitch()
+            expect(game.turn).to.equal(game.player2)
+        })
+    })
 })
